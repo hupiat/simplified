4ea7402b967ba5add4e3f968e870a274cefa8aa5
Hugo Piat <h.piat@outlook.fr>
2025-07-25 04:44:43 +0200

    Bot connection working

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
 dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+bot.start((ctx) => ctx.reply("Pump.fun bot ready!"));
+
+bot.launch();
